fceca85d9b71f514df04cbb1e67f398e4a0d2b14
robertorj7 <robertorj7@yahoo.com>
2025-07-25 15:42:53 -0300

    initial commit

---
+++ src/main/java/com/example/login_auth_api_kp/LoginAuthApiKpApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_auth_api_kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApiKpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApiKpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_auth_api_kp/LoginAuthApiKpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_auth_api_kp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiKpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
