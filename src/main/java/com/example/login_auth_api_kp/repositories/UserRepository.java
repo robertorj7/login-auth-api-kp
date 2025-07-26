706f06564831c56e051556e7ab717847b214ddd5
robertorj7 <robertorj7@yahoo.com>
2025-07-26 18:32:39 -0300

    created security filter

--- src/main/java/com/example/login_auth_api_kp/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api_kp/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.login_auth_api_kp.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String login);
 }
