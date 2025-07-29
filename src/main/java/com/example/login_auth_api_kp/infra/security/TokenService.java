4747f95c416c6fee818be583352c39f54002eeae
robertorj7 <robertorj7@yahoo.com>
2025-07-29 10:00:23 -0300

    Tokenservice correction

--- src/main/java/com/example/login_auth_api_kp/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api_kp/infra/security/TokenService.java
@@ -14,11 +14,9 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-
     @Value("${api.security.token.secret}")
     private String secret;
-
-    public String generateToken(User user) {
+    public String generateToken(User user){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
@@ -27,27 +25,25 @@ public class TokenService {
                     .withSubject(user.getEmail())
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
-
-        } catch (JWTCreationException exception) {
+        } catch (JWTCreationException exception){
             throw new RuntimeException("Error while authenticating");
         }
     }
 
-    public String validateToken(String token) {
+    public String validateToken(String token){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
             return JWT.require(algorithm)
                     .withIssuer("login-auth-api-kp")
                     .build()
                     .verify(token)
                     .getSubject();
         } catch (JWTVerificationException exception) {
-            throw null;
+            return null;
         }
     }
 
-    private Instant generateExpirationDate() {
+    private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
